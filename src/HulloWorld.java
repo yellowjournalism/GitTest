2fb8bffb34b11ec955a2d61d587631d8b2595843
yellowjournalism <kennywat@gmail.com>
2014-01-16 16:26:37 -0800

    Initial commit

---
+++ src/HulloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Created by kenny on 1/16/14.
+ */
+import java.util.*;
+
+public class HulloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("derp");
+    }
+}
